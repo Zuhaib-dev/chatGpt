77f922abbfeabaeb8ade4f2777dca6d7a7df8548
Zuhaib Rashid <xuvaxuva006@gmail.com>
2025-08-09 12:02:22 +0530

    done with the basic setup of the project

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
-const app = require('./src/app')
+const app = require("./src/app")
 app.listen(3000,()=>{
-    console.log("Server is running");
-    
+    console.log("server is running on port 3000")
 })
